64240467b2ec7f4f8a9676cf4f51dc200a8a3faf
Nauseouskibbles <greyson229@gmail.com>
2021-02-20 20:57:40 -0600

    Added bootstrap, added sperate list page, detected if user is logged in on main page and can show different stuff based off that, yeah, small update thouygh

--- RPGgame/routes.py
+++ RPGgame/routes.py
@@ -1,13 +1,18 @@
 from flask import render_template, url_for, redirect, request
-from RPGgame import app, db
+from RPGgame import app, db, bcrypt
 from RPGgame.forms import SignupForm, LoginForm
 from RPGgame.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
+
+
+# ONLY BORING STUFF BEYOND THIS POINT WARNING!!!!!! WARNING!!!!! -------------------------------------
 @app.route("/")
+@app.route('/home')
 def home():
     users = User.query.all()
-    return render_template("home.html", users=users)
+    anon = current_user.is_anonymous
+    return render_template("home.html", users=users, anon=anon)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -15,7 +20,8 @@ def signup():
         return redirect(url_for('home'))
     form = SignupForm()
     if form.validate_on_submit():
-        user = User(name=form.name.data, password=form.password.data)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(name=form.name.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
@@ -30,7 +36,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(name=form.name.data).first()
-        if user and user.password == form.password.data:
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
@@ -43,6 +49,11 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route("/list")
+def showlist():
+    users = User.query.all()
+    return render_template("list.html", users=users)
 
 @app.route("/create")
 def create():
