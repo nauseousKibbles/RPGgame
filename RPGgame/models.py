524519a82672d63d934cbee9b93cb121190f0530
Nauseouskibbles <greyson229@gmail.com>
2021-02-21 16:19:17 -0600

    Added more atributes to the user model, revamped navigation using bootstrap

--- RPGgame/models.py
+++ RPGgame/models.py
@@ -10,6 +10,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    health = db.Column(db.Integer, default=100)
+    defence = db.Column(db.Integer, default=1)
 
     def __repr__(self):
        return f"User('{self.name}', '{self.password}', '{self.id}')"
